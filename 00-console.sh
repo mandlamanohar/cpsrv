079c1355687914301a52120ca1c9512f9191641a
Manohar Lalith Kumar M <46880031+mandlamanohar@users.noreply.github.com>
2020-09-18 10:13:00 +0530

    Create 00-console.sh

---
+++ 00-console.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -e "************"
+echo -e "Console Menu"
+echo -e "************"
+
+echo -e "1 - Server Status"
+echo -e "2 - Server Status"
+echo -e "3 - Server Status"
+echo -e "4 - Server Status"
+
+echo -e "Select your option to continue:"
+read option
+
