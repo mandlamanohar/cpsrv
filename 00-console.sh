41134e819193f19a06110c207e26708fd528b7f6
Manohar Lalith Kumar M <46880031+mandlamanohar@users.noreply.github.com>
2020-09-18 10:14:49 +0530

    Update 00-console.sh

--- 00-console.sh
+++ 00-console.sh
@@ -10,5 +10,11 @@ echo -e "3 - Server Status"
 echo -e "4 - Server Status"
 
 echo -e "Select your option to continue:"
-read option
+read lhoption
 
+if [ "$lhoption" -eq "1" ] ;
+then
+echo -e "                             cPanel CPHULK Service        : Running"
+else
+echo -e "                             cPanel CPHULK Service        : Not Running"
+fi
